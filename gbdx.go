70ae7396206bdb2c5669c3abb4231d214b6768da
Pete Schmitt <pschmitt@gmail.com>
2016-04-13 14:44:04 -0600

    Fetch a token from the auth server.

--- gbdx.go
+++ gbdx.go
@@ -1,1 +1,12 @@
 package gbdx
+
+import "time"
+
+const GBDX_HTTP_TIMEOUT = 60 * time.Second
+
+type GBDXConfiguration struct {
+	Username       string
+	Password       string
+	ClientID       string
+	ClientPassword string
+}
