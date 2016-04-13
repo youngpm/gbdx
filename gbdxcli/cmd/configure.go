fe5beffe0553d08bcb368650342b4c119a7e4dbd
Patrick Young <patrick.mckendree.young@gmail.com>
2016-04-12 20:36:18 -0600

    Initial commit.

---
+++ gbdxcli/cmd/configure.go
@@ -0,0 +1,112 @@
+// Copyright © 2016 Patrick Young <patrick.mckendree.young@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type secretString string
+
+// String returns secretString types as a string with hidden entries.
+func (s secretString) String() (str string) {
+	for i, c := range s {
+		if i > 3 && len(s)-i < 5 {
+			str += string(c)
+		} else {
+			str += "*"
+		}
+	}
+	return
+}
+
+func configure(cmd *cobra.Command, args []string) (err error) {
+
+	var config GBDXConfiguration
+
+	// Get the configuration from the command line.
+	var configVars = []struct {
+		varName  string
+		prompt   string
+		val      *string
+		isSecret bool
+	}{
+		{"username", "GBDX User Name", &config.username, false},
+		{"password", "GBDX Password", &config.password, true},
+		{"clientID", "GBDX Client ID", &config.clientID, false},
+		{"clientPassword", "GBDX Client Password", &config.clientPassword, true},
+	}
+	for _, configVar := range configVars {
+		// Pretty print the prompt for this variable.
+		fmt.Printf(configVar.prompt)
+		if val := viper.GetString(configVar.varName); len(val) > 0 {
+			if configVar.isSecret {
+				fmt.Printf(" [%s]", secretString(val))
+			} else {
+				fmt.Printf(" [%s]", val)
+			}
+		}
+		fmt.Printf(": ")
+
+		// Read user input into the variable.  If an error is
+		// returned, we ignore it and just use the default, which is a
+		// nice way to handle the user just hitting enter when wanting to
+		// keep the default.
+		_, err := fmt.Scanln(configVar.val)
+		if err != nil {
+			*configVar.val = viper.GetString(configVar.varName)
+		}
+	}
+	fmt.Println(config)
+
+	return err
+}
+
+// configureCmd represents the configure command
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: configure,
+}
+
+func init() {
+	RootCmd.AddCommand(configureCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// configureCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// configureCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
---
+++ gbdxcli/cmd/util.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"os"
+	"path"
+	"runtime"
+)
+
+// ensureGBDXDir will create the gbdx directory if it doesn't already exist.
+func ensureGBDXDir() error {
+	return os.MkdirAll(path.Join(userHomeDir(), ".gbdx"), 0775)
+}
+
+// userHomeDir returns the home directory of the user.  I've borrowed
+// this from https://github.com/spf13/viper/blob/master/util.go .
+func userHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+// conf := &oauth2.Config{
+// 	ClientID:     "...",
+// 	ClientSecret: "...",
+// 	Endpoint: oauth2.Endpoint{
+// 		TokenURL: "https://geobigdata.io/auth/v1/oauth/token",
+// 	},
+// }
+
+// ctx := oauth2.NoContext
+// token, err := conf.PasswordCredentialsToken(ctx, "...", "...")
