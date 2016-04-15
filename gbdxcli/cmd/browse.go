75f45775e7b94eed3e84ac689413a2f23838f4a2
Patrick M Young <patrick.mckendree.young@gmail.com>
2016-04-15 16:02:58 -0600

    CLI help strings.

--- gbdxcli/cmd/browse.go
+++ gbdxcli/cmd/browse.go
@@ -56,14 +56,9 @@ func browse(cmd *cobra.Command, args []string) error {
 // browseCmd represents the browse command
 var browseCmd = &cobra.Command{
 	Use:   "browse",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	RunE: browse,
+	Short: "Download a browse",
+	Long:  `Download browse imagery.`,
+	RunE:  browse,
 }
 
 func init() {
