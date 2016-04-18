6ea3342a6ba446a5d73ab80bde427b9791596ddf
Patrick Young <patrick.mckendree.young@gmail.com>
2016-04-17 22:25:47 -0600

    Make gbdx directory only read/writeable by owner.
    This happens on creation, if its already existing it won't change the permissions.

--- gbdxcli/cmd/util.go
+++ gbdxcli/cmd/util.go
@@ -9,7 +9,7 @@ import (
 // ensureGBDXDir will create the gbdx directory if it doesn't already exist.
 func ensureGBDXDir() (string, error) {
 	gbdxPath := path.Join(userHomeDir(), ".gbdx")
-	err := os.MkdirAll(gbdxPath, 0775)
+	err := os.MkdirAll(gbdxPath, 0600)
 	return gbdxPath, err
 }
 
@@ -25,14 +25,3 @@ func userHomeDir() string {
 	}
 	return os.Getenv("HOME")
 }
-
-// conf := &oauth2.Config{
-// 	ClientID:     "...",
-// 	ClientSecret: "...",
-// 	Endpoint: oauth2.Endpoint{
-// 		TokenURL: "https://geobigdata.io/auth/v1/oauth/token",
-// 	},
-// }
-
-// ctx := oauth2.NoContext
-// token, err := conf.PasswordCredentialsToken(ctx, "...", "...")
